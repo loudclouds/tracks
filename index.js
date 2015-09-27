1ce8dc89ba5f8d7ab8343721ea4360fbfcbc9b94
loudclouds <loudclouds@users.noreply.github.com>
2015-09-26 22:58:19 -0500

    listening on ports is cool

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ var express = require('express');
 var data = require('./data.json');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => res.send(data));
-app.listen(3000, () => console.log('listening on port 3000'));
+app.listen(port, () => console.log(`listening on port ${port}`));
